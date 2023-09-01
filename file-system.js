9b5b685a674e326205f199cc672935f6eba88c52
lucasmoix <lmoix85@gmail.com>
2023-09-01 09:41:03 -0300

    hicimos el CRUD

--- file-system.js
+++ file-system.js
@@ -1,20 +1,46 @@
-const fs = require('fs'); //TODO: ver la otra forma con imports
+const fs = require("fs"); //TODO: ver la otra forma con imports
+const path = "./data/inventors.json";
 
-const path = './data/inventors.json';
 
-// cadena de texto
-let inventorsStr = fs.readFileSync(path, 'utf-8');
 
-// array de objetos
-let inventors = JSON.parse(inventorsStr);
+//escribir archivos
+let inventors = read();
 
-// TODO: escribir archivos
-inventors.push({first: 'Juan', last: 'Peres', year: 1956});
 
-// Create
-fs.writeFileSync(path, JSON.stringify(inventors, null, " "));
+//create
+
+
+console.log(inventors.filter((inventor) => inventor.first == "Albert"));
+
 
 // Ejercicio
 // TODO: CRUD Inventors Create Read Update Delete (o el ABM)
+function read(){
+   
+
+    // cadena de texto
+    let inventorsStr = fs.readFileSync(path, "utf-8");
+    
+    // array de objetos
+    let inventors = JSON.parse(inventorsStr);
+    return inventors
+}
+
+function write(){
+    fs.writeFileSync(path, JSON.stringify(inventors, null, " "));
+}
+
+function crear(inventor, inventors){
+    return inventors.push(inventor);
+}
+
+function actualizar(inventor, inventors){
+return inventors[inventors.findIndex(i=> i._id== inventor._id)] = inventor;
+}
+
+function borrar(id, inventors){
+    let index = inventors.findIndex(inventor => inventor._id== id);
+    return inventors.splice(index, 1);
+}
 
 //console.log(inventors.filter(inventor => inventor.first == 'Albert'));
