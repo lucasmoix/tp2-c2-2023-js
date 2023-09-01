4c2d30623e83cdaaff49ea8192b87196a5ff993b
Pablo Fernandez Hinojosa <ppfernandez@hiberus.com>
2023-08-31 21:52:01 -0300

    ejercicio json

--- Ejercicios 1.js
+++ Ejercicios 1.js
@@ -1,12 +1,12 @@
 /**
  * En el siguiente array de objetos (beers)
- * 1.- Desarrollar una function que retorne un array que incluya el precio segun el siguiente criterio
+ * 1.- (map) Desarrollar una function que retorne un array que incluya el precio segun el siguiente criterio
  *     a) Si el  grado alcoholico es < a 5.0 el precio es 300
  *     b) Si el grado alcoholico es >= 5.0 el precio es 350
  *     c) La cerveza 'Purple Iris' esta de oferta y si precio es 320
- * 2.- Desarrolle una función que inserte la propiedad file_name a cada uno de los objetos, esta propiedad debe tener solo
+ * 2.- (map y un split) Desarrolle una función que inserte la propiedad file_name a cada uno de los objetos, esta propiedad debe tener solo
  * el nombre del archivo de la propiedad label (upload_xOMnlK-large.png, etc..)
- * 3.- Desarrollo una funcion que ordene por precio y otra por tipo (type)
+ * 3.- (sort) Desarrollo una funcion que ordene por precio y otra por tipo (type)
  * 
  
  /*
--- file-system.js
+++ file-system.js
@@ -8,4 +8,13 @@ let inventorsStr = fs.readFileSync(path, 'utf-8');
 // array de objetos
 let inventors = JSON.parse(inventorsStr);
 
-console.log(inventors.filter(inventor => inventor.first == 'Albert'));
+// TODO: escribir archivos
+inventors.push({first: 'Juan', last: 'Peres', year: 1956});
+
+// Create
+fs.writeFileSync(path, JSON.stringify(inventors, null, " "));
+
+// Ejercicio
+// TODO: CRUD Inventors Create Read Update Delete (o el ABM)
+
+//console.log(inventors.filter(inventor => inventor.first == 'Albert'));
